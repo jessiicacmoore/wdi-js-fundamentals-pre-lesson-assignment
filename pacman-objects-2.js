65c746cd8cbbce048b7cf1ed93011be7984972bd
Jessica Moore <jessica.cmoore1@gmail.com>
2019-06-12 15:22:32 -0400

    Complete objects part 2

---
+++ pacman-objects-2.js
@@ -0,0 +1,7 @@
+const ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange' };
+console.log(ghosts);
+console.log(ghosts.inky);
+console.log(ghosts.blinky);
+
+console.log(ghosts['inky'])
+console.log(ghosts['blinky'])
