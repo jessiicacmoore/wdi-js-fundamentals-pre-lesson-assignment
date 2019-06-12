6105717d60a507eb32e058d1b91e985395715a30
Jessica Moore <jessica.cmoore1@gmail.com>
2019-06-12 15:11:08 -0400

    Complete arrays

---
+++ pacman-arrays.js
@@ -0,0 +1,9 @@
+const ghosts = ['Inky', 'Blinky', 'Pinky', 'Clyde'];
+
+console.log(`There are ${ghosts.length} ghosts.`); // backticks!
+
+console.log('Their names are: ');
+console.log(ghosts[0]);
+console.log(ghosts[1]);
+console.log(ghosts[2]);
+console.log(ghosts[3]);
